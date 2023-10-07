ee50391d145d695554ef8ee558e083b0b12177e4
WylieGlover <rg11445@georgiasouthern.edu>
2023-10-07 12:59:47 -0400

    fixed files

--- dataliteracy/dataliteracy/urls.py
+++ dataliteracy/dataliteracy/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path("", include("first.urls")),
+    path("first/", include("first.urls")),
     path("admin/", admin.site.urls),
-    path("", include("post.urls")),
 ]
--- dataliteracy/first/urls.py
+++ dataliteracy/first/urls.py
@@ -1,6 +1,7 @@
-from .views import index
 from django.urls import path
 
+from . import views
+
 urlpatterns = [
-    path("", index, name="index"),
+    path("", views.index, name="index"),
 ]
