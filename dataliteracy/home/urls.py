8cec57d2a0efc3190f6b6217eec169fa5f825744
WylieGlover <rg11445@georgiasouthern.edu>
2023-11-21 15:36:33 -0500

    new urls

--- dataliteracy/home/urls.py
+++ dataliteracy/home/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from . import views
 #
 urlpatterns = [
+    # Redirect from www.vizdataacademy.com to www.vizdataacademy.com/home
+    path('', RedirectView.as_view(url='/home', permanent=True), name='home_redirect'),
+    # Redirect from vizdataacademy.com to vizdataacademy.com/home
+    path('', RedirectView.as_view(url='/home', permanent=True), name='home_redirect_no_www'),
     path('home/', views.home, name='home'),
     path('serve-css/', views.serve_css, name='serve-css'),
     path('home/templates/contributors.html', views.contributors, name='contributors'),
