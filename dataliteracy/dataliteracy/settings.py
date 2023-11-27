29dd9612300d266e497473a727b99bccc2640540
WylieGlover <rg11445@georgiasouthern.edu>
2023-11-27 18:06:57 -0500

    fix

--- dataliteracy/dataliteracy/settings.py
+++ dataliteracy/dataliteracy/settings.py
@@ -73,8 +73,7 @@ TEMPLATES = [
                 "django.template.context_processors.debug",
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
-                "django.contrib.messages.context_processors.messages",
-                "django.middleware.csrf.CsrfViewMiddleware"
+                "django.contrib.messages.context_processors.messages"
             ],
         },
     },
