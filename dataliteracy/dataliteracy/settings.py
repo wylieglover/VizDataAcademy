e9272269c3215759f0e1f7a99299bccde7746af8
WylieGlover <rg11445@georgiasouthern.edu>
2023-11-27 18:04:57 -0500

    cerf middleware

--- dataliteracy/dataliteracy/settings.py
+++ dataliteracy/dataliteracy/settings.py
@@ -74,6 +74,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                "django.middleware.csrf.CsrfViewMiddleware"
             ],
         },
     },
