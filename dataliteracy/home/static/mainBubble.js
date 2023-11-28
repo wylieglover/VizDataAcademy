7420a945828ae30c176410b4ef8610a86fc86fd7
konecko923 <80303360+konecko923@users.noreply.github.com>
2023-11-27 21:28:32 -0500

    modified:   dataliteracy/home/static/css/classview.css 	modified:   dataliteracy/home/static/mainBubble.js 	modified:   dataliteracy/home/templates/bubbleGraphing.html

--- dataliteracy/home/static/mainBubble.js
+++ dataliteracy/home/static/mainBubble.js
@@ -1,64 +1,50 @@
-const Utils = {
-  months: ({ count }) => {
-    const monthLabels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-    return monthLabels.slice(0, count);
-  },
-  bubbles: ({ count, min, max }) => {
-    const bubbleData = Array.from({ length: count }, () => {
-      return {
-        x: Math.random() * (max - min) + min,
-        y: Math.random() * (max - min) + min,
-        r: Math.random() * (max - min) / 5,
-      };
-    });
-    return bubbleData;
-  },
-  CHART_COLORS: {
-    red: 'rgb(255, 99, 132)',
-    blue: 'rgb(54, 162, 235)',
-  },
-  transparentize: (color, alpha) => {
-    if (typeof color !== 'string' || !color.startsWith('rgb')) {
-      console.error('Invalid color provided to transparentize:', color);
-      return color; 
-    }
-    return color.replace('rgb', 'rgba').replace(')', `, ${alpha})`);
-  },
+const config = {responsive: true};
+
+var trace1 = {
+  x: [1, 2, 3, 4],
+  y: [10, 11, 12, 13],
+  mode: 'markers',
+  marker: {
+    size: [40, 60, 80, 100]
+  }
+}
+
+var data = [trace1];
+
+var layout = {
+  title: 'Marker Size',
+  showlegend: false
 };
 
-const ctx = document.getElementById('bubblegraph');
-const DATA_COUNT = 7;
-const NUMBER_CFG = {count: DATA_COUNT, min: -100, max: 100};
-const labels = Utils.months({count: 7});
-  new Chart(ctx, {
-    type: 'bubble',
-    data: {
-      labels: labels,
-      datasets: [
-        {
-          label: 'Dataset 1',
-          data: Utils.bubbles(NUMBER_CFG),
-          borderColor: Utils.CHART_COLORS.red,
-          backgroundColor: Utils.transparentize(Utils.CHART_COLORS.red, 0.5),
-        },
-        {
-          label: 'Dataset 2',
-          data: Utils.bubbles(NUMBER_CFG),
-          borderColor: Utils.CHART_COLORS.blue,
-          backgroundColor: Utils.transparentize(Utils.CHART_COLORS.blue, 0.5),
-        }
-      ]
-    },
-    options: {
-      responsive: true,
-      plugins: {
-        legend: {
-          position: 'top',
-        },
-        title: {
-          display: true,
-          text: 'Chart.js Bubble Chart'
-        }
-      }
-    }
-  });
+Plotly.newPlot('useGraph', data, layout, config);
+
+function updateBars(_this) {
+  var ItemCount = +_this.value
+  var results = document.querySelector('#barNames')
+  results.innerHTML = '' //clear the results on each update
+for (var i = 1; i <= ItemCount; i++) {
+  var input = document.createElement('input') //create input
+  var label = document.createElement("label") //create label
+  var inputR = document.createElement('input')
+  var inputY = document.createElement('input')
+  label.innerText = 'Name ' + i
+  //label.className = "inputText"
+  input.type = "text";
+  input.placeholder = "X Value"; //add a placeholder
+  input.className = "inputText"; // set the CSS class
+  input.id = "nameNum" + i;
+  inputY.type = "text";
+  inputY.placeholder = "Y Value"; //add a placeholder
+  inputY.className = "inputText"; // set the CSS class
+  inputY.id = "nameNumY" + i;
+  inputR.type = "text";
+  inputR.placeholder = "Point Weight"; //add a placeholder
+  inputR.className = "inputText"; // set the CSS class
+  inputR.id = "nameNumR" + i;
+  results.appendChild(label); //append label
+  results.appendChild(input); //append input
+  results.appendChild(inputY)
+  results.appendChild(inputR)
+  results.appendChild(document.createElement("br"));
+}
+}
