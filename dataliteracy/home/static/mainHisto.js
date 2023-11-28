bff8a305ce286c8e3dd36008a627c8b17c63de46
WylieGlover <rg11445@georgiasouthern.edu>
2023-11-27 21:48:35 -0500

    histograph functionality

--- dataliteracy/home/static/mainHisto.js
+++ dataliteracy/home/static/mainHisto.js
@@ -1,20 +1,68 @@
-const ctx = document.getElementById('histogram');
+const config = { responsive: true };
+var x = [];
+for (var i = 0; i < 30; i++) {
+  x[i] = Math.random() * 100;
+}
 
-  new Chart(ctx, {
+var trace = {
+  x: x,
+  type: 'histogram',
+};
+var layout = {
+  bargap: 0.05,
+  bargroupgap: 0.2,
+  barmode: "overlay",
+  title: "Sampled Results",
+  xaxis: { title: "Value" },
+  yaxis: { title: "Count" }
+};
+var data = [trace];
+Plotly.newPlot('useGraph', data, layout);
+
+
+function updateBars() {
+  const numBars = parseInt(document.getElementById('numBars').value);
+  const barNamesContainer = document.getElementById('barNamesInput');
+  const barValuesContainer = document.getElementById('barValuesInput');
+
+  barNamesContainer.innerHTML = ''; // Clear previous inputs
+  barValuesContainer.innerHTML = '';
+
+  for (let i = 0; i < numBars; i++) {
+    const barNameInput = document.createElement('input');
+    barNameInput.type = 'text';
+    barNameInput.placeholder = `Bar ${i + 1} Name`;
+    barNamesContainer.appendChild(barNameInput);
+
+    const barValueInput = document.createElement('input');
+    barValueInput.type = 'number';
+    barValueInput.placeholder = `Bar ${i + 1} Value`;
+    barValuesContainer.appendChild(barValueInput);
+  }
+}
+
+function submitDataBar() {
+  const barNamesInputs = document.getElementById('barNamesInput').querySelectorAll('input');
+  const barValuesInputs = document.getElementById('barValuesInput').querySelectorAll('input');
+
+  const barNames = Array.from(barNamesInputs).map(input => input.value);
+  const barValues = Array.from(barValuesInputs).map(input => parseFloat(input.value));
+
+  // Use barNames and barValues to update your Plotly trace or perform other actions as needed
+  console.log('Bar Names:', barNames);
+  console.log('Bar Values:', barValues);
+
+  const data = [{
+    x: barNames,
+    y: barValues,
     type: 'bar',
-    data: {
-      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-      datasets: [{
-        label: '# of Votes',
-        data: [12, 19, 3, 5, 2, 3],
-        borderWidth: 1
-      }]
-    },
-    options: {
-      scales: {
-        y: {
-          beginAtZero: true
-        }
-      }
-    }
-  });
+    // other properties specific to your plot
+  }];
+
+  const layout = {
+    title: document.getElementById('chartName').value || 'Graph Title',
+    // other layout properties specific to your plot
+  };
+
+  Plotly.newPlot('useGraph', data, layout);
+}
